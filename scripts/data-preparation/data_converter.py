d40d287c105471dff35ee1d3c7d5519317134cab
Uga Sprogis <ugasprogis12@inbox.lv>
2020-09-19 13:04:00 +0300

    added README

--- scripts/data-preparation/data_converter.py
+++ scripts/data-preparation/data_converter.py
@@ -1,90 +1,81 @@
 import mysql.connector as mysql
+import sys,json
 
-db = mysql.connect(user='root', password='root',host='localhost',database='twitter')
+"""
+create table tweets1 as select * from tweets left join media on tweets.id = media.tweet_id;
+create table tweets2 as select * from tweets1 left join vietas on tweets1.geo = vietas.nosaukums;
+ALTER TABLE tweets2 ORDER BY created_at ASC;
+create table tweets3 as select * from tweets2 t left join (SELECT distinct(tvits) FROM words o) AS o ON o.tvits = t.id;
+"""
 
+
+db = mysql.connect(user='root', password='root',host='localhost',database='july_import', port='3306')
 mycursor = db.cursor()
 
-mycursor.execute("SELECT * FROM tweets2013Q1")
+db1 = mysql.connect(user='root', password='root',host='localhost',database='july_import', port='3306')
+mycursor1 = db1.cursor()
 
-x = open("tweets2013Q1.json", "w")
-cot = 1
+if len(sys.argv) > 1 and sys.argv[1] > 2010 and sys.argv[1] < 2021:
+    print(sys.argv[1])
+    mycursor.execute("SELECT * FROM tweets3 where YEAR(created_at) = '"+sys.argv[1]+"'")
+else:
+    mycursor.execute("SELECT id, text, screen_name, created_at, media_url, expanded_url, nosaukums, lng, lat, valsts FROM tweets3")
+
+x = open("tweets_latest.json", "w")
 x.write("[\n")
 
-results = mycursor.fetchone()
-while results != None:
-	x.write("    {\n")
-	x.write("      " + "\"tweet_id\":  " + str(results[0]) + ",\n")
-	x.write("      " + "\"tweet_text\":  " + "\"" + str(results[1]).replace("\n","") + "\"" + ",\n")
-	x.write("      " + "\"tweet_author\":  " + "\"" + str(results[2]) + "\"" + ",\n")
-	x.write("      " + "\"created_at\":  " + "\"" + str(results[3]) + "\"" + ",\n")
-	if str(results[7]) == "None":
-		x.write("      " + "\"media_url\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"media_url\":  " + "\"" + str(results[7]) + "\"" + ",\n")
-	if str(results[8]) == "None":
-		x.write("      " + "\"expanded_url\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"expanded_url\":  " + "\"" + str(results[8]) + "\"" + ",\n")
-	if str(results[10]) == "None":
-		x.write("      " + "\"location_name\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"location_name\":  " + "\"" + str(results[10]) + "\"" + ",\n")
-	if str(results[11]) == "None":
-		x.write("      " + "\"location_lng\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"location_lng\":  " + "\"" + str(results[11]) + "\"" + ",\n")
-	if str(results[12]) == "None":
-		x.write("      " + "\"location_lat\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"location_lat\":  " + "\"" + str(results[12]) + "\"" + ",\n")
-	if str(results[13]) == "None":
-		x.write("      " + "\"location_country\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"location_country\":  " + "\"" + str(results[13]) + "\"" + ",\n")
+for results in mycursor:
 
+    tweet_dict = {}
+    
+    if str(results[4]) != "None":
+        tweet_dict["media_url"] = str(results[4])
+    if str(results[5]) != "None":
+        tweet_dict["expanded_url"] = str(results[5])
+    if str(results[6]) != "None":
+        tweet_dict["location_name"] = str(results[6])
+    if str(results[7]) != "None":
+        tweet_dict["location_lng"] = str(results[7])
+    if str(results[8]) != "None":
+        tweet_dict["location_lat"] = str(results[8])
+    if str(results[9]) != "None":
+        tweet_dict["location_country"] = str(results[9])
+    
+    fsfp = ""
+    fnfp = ""
+    fgp = ""
+    fetp = ""
+    fsf = ""
+    fnf = ""
+    fg = ""
+    fet = ""
+    
+    search = (results[0],)
+    sql = "select * from words where tvits = %s"
+    mycursor1.execute(sql,search);
+    res = mycursor1.fetchall()
+    
+    if str(res) != "None" and res != []:
+        for r in res:
+            fsfp += str(r[0]) + ";"
+            fnfp += str(r[1]) + ";"
+            fgp += str(r[4]) + ";"
+            fetp += str(r[5]) + ";"
 
-
-	db1 = mysql.connect(user='root', password='root',host='localhost',database='twitter')
-	mycursor1 = db1.cursor()
-	search = (results[0],)
-	sql = "select * from words2013Q1 where tvits = %s"
-	mycursor1.execute(sql,search);
-	res = mycursor1.fetchall()
-
-	if str(res) == "None" or res == []:
-		x.write("      " + "\"food_surface_form\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"food_surface_form\":  " + "\"")
-		for r in res:
-			x.write(str(r[0]) + ";")
-		x.write("\"" + ",\n")
-
-	if str(res) == "None" or res == []:
-		x.write("      " + "\"food_nominative_form\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"food_nominative_form\":  " + "\"")
-		for r in res:
-			x.write(str(r[1]) + ";")
-		x.write("\"" + ",\n")
-
-	if str(res) == "None" or res == []:
-		x.write("      " + "\"food_group\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"food_group\":  " + "\"")
-		for r in res:
-			x.write(str(r[4]) + ";")
-		x.write("\"" + ",\n")
-
-	if str(res) == "None" or res == []:
-		x.write("      " + "\"food_english_translation\":  " + "\"""\"" + ",\n")
-	else:
-		x.write("      " + "\"food_english_translation\":  " + "\"")
-		for r in res:
-			x.write(str(r[5]) + ";")
-		x.write("\"" + ",\n")	
-	x.write("    },\n")
-	print(cot)
-	cot = cot + 1
+        tweet_dict["food_surface_form"] = fsfp
+        tweet_dict["food_nominative_form"] = fnfp
+        tweet_dict["food_group"] = fgp
+        tweet_dict["food_english_translation"] = fetp
+       
+    #Write everything only once.
+    tweet_dict["tweet_id"] = str(results[0])
+    tweet_dict["tweet_text"] = str(results[1]).replace("\n","")
+    tweet_dict["tweet_author"] = str(results[2])
+    tweet_dict["created_at"] = str(results[3])
+    
+    output = json.dumps(tweet_dict, sort_keys=True, ensure_ascii=False)
+    
+    x.write(output.strip() + ",\n")
 
 x.write("]")
 
